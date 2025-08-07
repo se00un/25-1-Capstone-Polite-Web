36a43562784267c9f6d74b3774ddbf4b1c0432a6
박서은 <seoeun@bagseoeun-ui-MacBookPro.local>
2025-08-08 01:03:59 +0900

    마지막 수정

--- polite_back/database.py
+++ polite_back/database.py
@@ -8,10 +8,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_async_engine(
     DATABASE_URL,
     echo=True,
-    connect_args={"ssl": True}  
+    connect_args={"ssl": True},
+    pool_pre_ping=True
 )
 
 async_session = sessionmaker(
