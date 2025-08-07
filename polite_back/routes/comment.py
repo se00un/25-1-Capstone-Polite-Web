c399d0539165c91753e713a9725691e451cf7a38
박서은 <seoeun@bagseoeun-ui-MacBookPro.local>
2025-08-07 23:08:06 +0900

    reply_to 관련 수정

--- polite_back/routes/comment.py
+++ polite_back/routes/comment.py
@@ -1,19 +1,18 @@
 # polite_back/routes/comment.py
-# 수정 확인!! 
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from polite_back import model
 from polite_back.database import get_db
-from polite_back.schemas.schemas import CommentCreate, CommentOut
+from polite_back.schemas.schemas import CommentCreate
 from datetime import datetime, timedelta
 
 KST = timedelta(hours=9)
 
 router = APIRouter()
 
-@router.post("/comments/add", response_model=CommentOut)
+@router.post("/comments/add")
 async def add_comment(comment: CommentCreate, db: AsyncSession = Depends(get_db)):
     new_comment = model.Comment(
         user_id=comment.user_id,
@@ -25,22 +24,25 @@ async def add_comment(comment: CommentCreate, db: AsyncSession = Depends(get_db)
         selected_version=comment.selected_version,
         reply_to=comment.reply_to,
         is_modified=comment.is_modified,
-        created_at = (datetime.utcnow() + KST).replace(tzinfo=None),
+        created_at=(datetime.utcnow() + KST).replace(tzinfo=None),
     )
 
     db.add(new_comment)
     await db.commit()
     await db.refresh(new_comment)
-    return new_comment
 
-@router.get("/comments/{post_id}", response_model=list[CommentOut])
+    # FastAPI가 response_model 쓰지 않고 dict로 보냄 → 오류 방지
+    return {
+        "message": "success",
+        "comment": new_comment.__dict__
+    }
+
+
+@router.get("/comments/{post_id}")
 async def get_comments_by_post(post_id: int, db: AsyncSession = Depends(get_db)):
     result = await db.execute(
         select(model.Comment).where(model.Comment.post_id == post_id)
     )
-    return result.scalars().all()
+    flat_comments = result.scalars().all()
 
-@router.get("/comments", response_model=list[CommentOut])
-async def get_all_comments(db: AsyncSession = Depends(get_db)):
-    result = await db.execute(select(model.Comment))
-    return result.scalars().all()
+    return [c.__dict__ for c in flat_comments] 
